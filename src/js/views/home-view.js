102fbb34eb73025f6bd3227870ea25463ed776ef
fr4nbtt <batet.francisco@gmail.com>
2025-04-12 22:10:23 -0300

    chore: Remove unused console.logs and comments in home-view.js

--- src/js/views/home-view.js
+++ src/js/views/home-view.js
@@ -5,10 +5,9 @@ import { renderProjects } from "../dom-render.js";
 
 // Function to render the Home view
 export function renderHomeView() {
-  console.log("Rendering Home view...");
   const main = document.querySelector("main");
-  main.innerHTML = ""; // Clear the main content area
+  main.innerHTML = "";
 
   const projects = getProjects();
-  renderProjects(projects); // Render the projects
+  renderProjects(projects);
 }
